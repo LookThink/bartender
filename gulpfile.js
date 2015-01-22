220f2a18ebe86898037b2f657715717960b4758e
Greg Bruening <greg@lookthink.com>
2015-01-22 15:43:50 -0500

    Initial project push

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+// Call required plugins
+var gulp = require("gulp"),
+    del = require("del");
+
+
+// Set project paths
+var paths = {
+  src: "src/",
+  dest: "app/"
+};
+
+
+// Build pipes
+
+gulp.task("default", function() {
+  
+});
---
+++ readme.md
@@ -0,0 +1,16 @@
+#Barkeep
+##LookThink front-end project management tool
+
+##Included operations
+
++ del
++ autoprefixer
++ cached
++ imagemin
++ livereload
++ minify-css
++ notify
++ remember
++ ruby-sass
++ sass
++ scss-lilnt
