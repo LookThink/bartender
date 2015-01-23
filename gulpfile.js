b636ea55e039ba64eed343e3a90810d0eb71a5c7
Greg Bruening <greg@lookthink.com>
2015-01-23 16:11:24 -0500

    Create styles task; sourcemap, lint, prefix, compile, minify

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
 // Call required plugins
 var gulp = require("gulp"),
     sourcemaps = require("gulp-sourcemaps"),
-    sass = require("gulp-sass");
+    autoprefixer = require("gulp-autoprefixer"),
+    scsslint = require("gulp-scss-lint"),
+    sass = require("gulp-sass"),
+    minifyCSS = require("gulp-minify-css");
 
 
 // Set project paths
@@ -12,17 +15,32 @@ var paths = {
 
 
 // Build pipes
-// Styles (Sass Compiling / Minification)
+// Styles
 gulp.task("styles", function() {
   return gulp.src(paths.src + "scss/**/*.scss")
+    // Initialize sourcemapping
     .pipe(sourcemaps.init())
+      // Run SCSS Lint on all SCSS files
+      .pipe(scsslint({
+        config: "default.yml",
+        reporterOutput: "scssReport.xml"
+      }))
+      // Compile SCSS
       .pipe(sass({
         errLogToConsole: true
       }))
+      // Autoprefix compiled CSS
+      .pipe(autoprefixer({
+        browsers: ["last 2 versions", "Explorer >= 8"],
+        cascade: false
+      }))
+      // Minify compiled CSS
+      .pipe(minifyCSS())
+    // Create the sourcemap
     .pipe(sourcemaps.write())
     .pipe(gulp.dest(paths.dest + "css"));
 });
 
 gulp.task("default", function() {
-  
+  gulp.start("styles");
 });
