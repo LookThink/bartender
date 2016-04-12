cc5a6864d58a5113785d80171f09e0f4eea2a1f3
Aaron Wentt <aaron.w@lookthink.com>
2016-04-11 23:31:47 -0400

    added task that handles the static files, such as favicons, json, etc.

---
+++ gulp/tasks/static.js
@@ -0,0 +1,21 @@
+var yaml   = require('yamljs'),
+    config = yaml.load('./gulp/config.yml'),
+    changed = require('gulp-changed'),
+    gulp    = require('gulp'),
+    path    = require('path')
+;
+
+var paths = {
+  src: path.join(config.root.src, config.tasks.static.src, '/**'),
+  dest: path.join(config.root.dest, config.tasks.static.dest)
+};
+
+var staticTask = function() {
+  return gulp.src(paths.src)
+    .pipe(changed(paths.dest)) // Ignore unchanged files
+    .pipe(gulp.dest(paths.dest))
+  ;
+};
+
+gulp.task('static', staticTask);
+module.exports = staticTask;
