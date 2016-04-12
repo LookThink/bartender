058141b4c24dfec7b1ff8dcdbe84d9de6534127b
Aaron Wentt <aaron.w@lookthink.com>
2016-04-11 23:31:08 -0400

    added production task that prepares dest for deployment

---
+++ gulp/tasks/production.js
@@ -0,0 +1,17 @@
+var yaml   = require('yamljs'),
+    config = yaml.load('./gulp/config.yml'),
+    gulp         = require('gulp'),
+    gulpSequence = require('gulp-sequence'),
+    getEnabledTasks = require('../lib/getEnabledTasks')
+;
+
+var productionTask = function(cb) {
+    global.production = true;
+    
+    var tasks = getEnabledTasks('production');
+    
+    gulpSequence('clean', tasks.assetTasks, tasks.codeTasks, 'static', cb);
+};
+
+gulp.task('production', productionTask);
+module.exports = productionTask;
