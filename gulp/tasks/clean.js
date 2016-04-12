f1cd12ea473615c8f8e6f274f6d81b2aa30b0008
Aaron Wentt <aaron.w@lookthink.com>
2016-04-11 23:29:33 -0400

    added task that cleans dest folder

---
+++ gulp/tasks/clean.js
@@ -0,0 +1,14 @@
+var gulp   = require('gulp'),
+    del    = require('del'),
+    yaml   = require('yamljs')
+    config = yaml.load('./gulp/config.yml')
+;
+
+var cleanTask = function (cb) {
+  del([config.root.dest]).then(function (paths) {
+    cb();
+  });
+};
+
+gulp.task('clean', cleanTask);
+module.exports = cleanTask;
