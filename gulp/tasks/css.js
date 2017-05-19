4242c69fb6a5b519a05be87bfc19f9d852ee238d
Greg Bruening <greg@lookthink.com>
2017-05-19 11:52:06 -0400

    Update CSS task to include better PostCSS integrations

--- gulp/tasks/css.js
+++ gulp/tasks/css.js
@@ -5,14 +5,16 @@ var yaml         = require('yamljs'),
 if (!config.tasks.css) return;
 
 var gulp         = require('gulp'),
+    path         = require('path'),
     browserSync  = require('browser-sync'),
     handleErrors = require('../lib/handleErrors'),
     sass         = require('gulp-sass'),
     sourcemaps   = require('gulp-sourcemaps'),
-    postCss      = require('gulp-postcss'),
-    autoprefixer = require('autoprefixer'),
-    cssnano      = require('cssnano'),
-    path         = require('path')
+    postcss      = require('gulp-postcss'),
+    cssnext      = require('postcss-cssnext'),
+    assets       = require('postcss-assets'),
+    colorguard   = require('gulp-colorguard'),
+    cssnano      = require('gulp-cssnano')
 ;
 
 var paths = {
@@ -21,10 +23,15 @@ var paths = {
 };
 
 // define postcss tasks
-var autoprefixer  = autoprefixer(config.tasks.css.postCss.autoprefixer),
-    cssnano       = cssnano(config.tasks.css.postCss.cssnano),
+var cssnext       = cssnext(config.tasks.css.postcss.cssnext),
+    assets        = assets(config.tasks.css.postcss.assets),
+    colorguard    = colorguard(config.tasks.css.postcss.colorguard),
+    cssnano       = cssnano(config.tasks.css.postcss.cssnano),
     postcssConfig = [
-      autoprefixer,
+      cssnext,
+      assets,
+      colorguard,
+      //  TODO - Only run cssnano in Prod, not Dev
       cssnano
     ]
 ;
@@ -32,19 +39,19 @@ var autoprefixer  = autoprefixer(config.tasks.css.postCss.autoprefixer),
 // CSS
 // Compile SCSS files and run PostCSS on output
 // PostCSS handles autoprefixing and minification
-var processScss = function() {
+var processCSS = function() {
   // Actual task
   return gulp.src(paths.src)
     .pipe(sourcemaps.init())
-    .pipe(sass()
-    .on('error', handleErrors))
-    .pipe(postCss(postcssConfig))
+    .pipe(sass())
+      .on('error', handleErrors))
+    .pipe(postcss(postcssConfig))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest(paths.dest))
     .pipe(browserSync.stream())
   ;
 };
 
-gulp.task('css', processScss);
+gulp.task('css', processCSS);
 
-module.exports = processScss;
+module.exports = processCSS;
